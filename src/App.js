ec044c66251e495ac3a775ea4aa74d1883de5636
N01552868 <n01552868@humber.ca>
2023-10-30 20:54:19 -0400

    Added paragraph to test deployment changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hey There
-      </header>
+      <header>Ethan Ly</header>
+      <h2>Computer Programmer and Analysis Student</h2>
+      <p>
+        Hi my name is Ethan. As a computer Programmer I'm drive to learn anything about programming and love putting long amounts into coding 
+        itself. The most frustrating part about coding is solving the problems we as programmers face, which is why to me it's always
+        so satifying to me when a problem is solved or when I finally understand something new. No matter where I am in my coding journey
+        I hope to continue solving problems and learning new things in coding. 
+      </p>
     </div>
   );
 }
